004c2f0a8d945bfb7dbe16156a2ceeedc7b6707e
Ysabelle Castro <rochaysabelle@hotmail.com>
2023-12-12 09:07:02 +0000

    teste inicial da atvidade

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,27 @@
+alert('Olá mundo');
+let numeroMaximo = 1000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+
+// enquanto chute não for igualç ao n.s
+while (chute != numeroSecreto) {
+    chute = prompt(`escolha um número entre 1 e ${numeroMaximo}`);
+// se chute for igual ao número secreto
+if (chute == numeroSecreto) {
+    break;
+} else {
+    if (chute > numeroSecreto) {alert(`O número secreto é menor ${chute}`);
+} else { alert(`O número secreto é maior que ${chute}`);}
+tentativas++;
+}
+}
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`isso aí! você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
+
+
+//if (tentativas > 1) {
+//    alert(`isso aí! você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+//} else {alert(`isso aí! você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa`);}
